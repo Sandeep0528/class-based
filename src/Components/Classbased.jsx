58bdc79d9bf92497810dfe97c7653bc4815123ab
unknown <challasandeep05@gmail.com>
2025-03-15 21:51:18 +0530

    files commited

---
+++ src/Components/Classbased.jsx
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+class Classbased extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 0
+        }
+        this.myFunction = this.myFunction.bind(this)
+    }
+
+    myFunction = () => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+    render() {
+        return (
+            <div style={{ textAlign: "center", margin: "auto", background: "blue" }}>
+                <h1 style={{ color: "red" }}>
+                    Counting App
+                </h1>
+                <p>hello from counting app</p>
+                <p>Counter:{this.state.counter}</p>
+                <button onClick={this.myFunction}> cliq</button>
+            </div>
+
+
+
+        )
+    }
+}
+
+export default Classbased
